d94a4371a0ef4fcc74f05f69eee7f773692ca2da
Shreyas Kapur <shreyask@mit.edu>
2017-05-25 05:51:34 -0400

    Init

---
+++ main.py
@@ -0,0 +1,5 @@
+from rfp_service import *
+from secret import *
+
+if __name__ == '__main__':
+	submit_rfp(USERNAME, PASSWORD, DUO_SECRET, 0, 'Test', None)
---
+++ rfp_service.py
@@ -0,0 +1,2 @@
+def submit_rfp(kerb_user, kerb_password, duo_secret, duo_count, rfp_name, items):
+	pass
